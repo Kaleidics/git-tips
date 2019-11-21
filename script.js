52bc5155af9865449bb6c527f77d6664f1edfad6
Eddie Chu <contact@eddiechu.me>
2019-11-20 23:17:54 -0800

    update content

---
+++ script.js
@@ -0,0 +1,7 @@
+// Tip1
+const tip_1 = document.querySelector("#tip_1");
+const tip_1_content = document.querySelector("#tip_1_content");
+tip_1.addEventListener("click", function() {
+  tip_1_content.classList.toggle("hidden");
+});
+
