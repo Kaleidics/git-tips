76a309cd04780d2edcf00748511d40549af4fe21
Eddie Chu <contact@eddiechu.me>
2019-11-21 00:06:02 -0800

    add a tip

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ tip_1.addEventListener("click", function() {
   tip_1_content.classList.toggle("hidden");
 });
 
+const tip_2 = document.querySelector("#tip_2");
+const tip_2_content = document.querySelector("#tip_2_content");
+tip_2.addEventListener("click", function() {
+  tip_2_content.classList.toggle("hidden");
+});
+
