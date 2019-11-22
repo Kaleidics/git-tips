f35178afc023e14eb141f73deb05e557e21a893a
Eddie Chu <contact@eddiechu.me>
2019-11-21 23:07:00 -0800

    add new tips

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-triggerOn("#tip_1", "#tip_1_content");
-triggerOn("#tip_2", "#tip_2_content");
-triggerOn("#tip_3", "#tip_3_content");
-triggerOn("#tip_4", "#tip_4_content");
+triggerOn('#tip_1', '#tip_1_content');
+triggerOn('#tip_2', '#tip_2_content');
+triggerOn('#tip_3', '#tip_3_content');
+triggerOn('#tip_4', '#tip_4_content');
+triggerOn('#tip_5', '#tip_5_content');
+triggerOn('#tip_6', '#tip_6_content');
 
 function triggerOn(option, target) {
-  const tip = document.querySelector(option);
-  const tipContent = document.querySelector(target);
+    const tip = document.querySelector(option);
+    const tipContent = document.querySelector(target);
 
-  tip.addEventListener("click", function() {
-    tipContent.classList.toggle("hidden");
-  });
+    tip.addEventListener('click', function() {
+        tipContent.classList.toggle('hidden');
+    });
 }
+
+
