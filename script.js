5c6b9dae4fe005c4830bd55f91d37202f8f00ede
Eddie Chu <contact@eddiechu.me>
2019-11-22 00:06:57 -0800

    force refresh

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-triggerOn('#tip_1', '#tip_1_content');
-triggerOn('#tip_2', '#tip_2_content');
-triggerOn('#tip_3', '#tip_3_content');
-triggerOn('#tip_4', '#tip_4_content');
-triggerOn('#tip_5', '#tip_5_content');
-triggerOn('#tip_6', '#tip_6_content');
+let refresh = window.localStorage.getItem('refresh');
+if (refresh === null) {
+    window.location.reload();
+    window.localStorage.setItem('refresh', '1');
+}
+
+window.addEventListener('unload', function() {
+    localStorage.removeItem('refresh');
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    triggerOn('#tip_1', '#tip_1_content');
+    triggerOn('#tip_2', '#tip_2_content');
+    triggerOn('#tip_3', '#tip_3_content');
+    triggerOn('#tip_4', '#tip_4_content');
+    triggerOn('#tip_5', '#tip_5_content');
+    triggerOn('#tip_6', '#tip_6_content');
+});
 
 function triggerOn(option, target) {
     const tip = document.querySelector(option);
@@ -13,5 +25,3 @@ function triggerOn(option, target) {
         tipContent.classList.toggle('hidden');
     });
 }
-
-
